a50cec5bb8ab38b49cb7c6e0706307359c6660b9
Ellinor554 <tano.ellinor@gmail.com>
2026-05-03 20:47:53 +0200

    Add ToolContract.js with ToolModule definition
    Define the structure of a ToolModule and export the contract version.

---
+++ modules/core/ToolContract.js
@@ -0,0 +1,11 @@
+// A ToolModule is any object shaped like:
+//   {
+//     id:    'volym',
+//     title: 'Volym',
+//     mount(parentEl):  HTMLElement,
+//     onEnter?():       void,
+//     onLeave?():       void,
+//     headerControls?(): HTMLElement[]
+//   }
+
+export const TOOL_CONTRACT_VERSION = 1;
