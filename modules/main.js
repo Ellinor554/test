71eddd2851959d161a8448b74790a9e531c95842
Ellinor554 <tano.ellinor@gmail.com>
2026-05-04 15:44:10 +0200

    Add new tools to the ToolRegistry

--- modules/main.js
+++ modules/main.js
@@ -1,7 +1,9 @@
-import { ToolRegistry }     from './core/ToolRegistry.js';
-import { createHomeTool }   from './tools/HomeTool.js';
-import { createVolumeTool } from './tools/VolumeTool.js';
-import { createClockTool }  from './tools/ClockTool.js';
+import { ToolRegistry }          from './core/ToolRegistry.js';
+import { createHomeTool }        from './tools/HomeTool.js';
+import { createVolumeTool }      from './tools/VolumeTool.js';
+import { createClockTool }       from './tools/ClockTool.js';
+import { createNumberLineTool }  from './tools/NumberLineTool.js';
+import { createCountingTool }    from './tools/CountingTool.js';
 
 const registry = new ToolRegistry({
     mountEl:      document.getElementById('tool-mount'),
@@ -13,6 +15,8 @@ const registry = new ToolRegistry({
 registry.register(createHomeTool());
 registry.register(createVolumeTool());
 registry.register(createClockTool());
+registry.register(createNumberLineTool());
+registry.register(createCountingTool());
 
 registry.mountActive();
 
